c6b1286e6dd6ad519c9cb5623f19b0f8dc0be1f0
mdjewel999 <mdjewel878761@gmail.com>
2023-03-28 01:00:12 +0600

    added product code

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import './Product.css';
-
+import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
-    const {img, name, seller, quantity, price} = props.product;
-    return (
-        <div className='product'>
-           <img src={img} alt="" />
-           <h6>{name}</h6>
-        </div>
-    );
+  const { img, name, seller, ratings, price } = props.product;
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <p>Price: {price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings} Star</p>
+      </div>
+      <button className="btn-cart">Add To Cart</button>
+    </div>
+  );
 };
 
 export default Product;
